6eaeb9a7b0e81eb78f8996a96684ce078071d541
Luiz Ludovico <luiz_ludovico00@hotmail.com>
2025-06-18 19:42:03 -0300

    chore: limpa arquivos ignorados e aplica novo .gitignore e ajuste na program

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,4 +1,5 @@
 using CrossCutting.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +10,15 @@ ConfigureRepository.ConfigureDependenciesRepository(builder.Services);
 // MVC + Swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Arcade Score",
+        Version = "v1",
+        Description = "API para registrar e analisar pontuações em partidas de fliperama."
+    });
+});
 
 var app = builder.Build();
 
